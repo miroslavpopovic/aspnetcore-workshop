1218984c5358a6f5cc062a567085f68737f77e30
Miroslav Popovic <miroslav.popovic@gmail.com>
2019-07-15 01:06:35 +0200

    Add validation for UserInputModel

--- src/TimeTracker.Client/Models/UserInputModel.cs
+++ src/TimeTracker.Client/Models/UserInputModel.cs
@@ -1,8 +1,13 @@
-﻿namespace TimeTracker.Client.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeTracker.Client.Models
 {
     public class UserInputModel
     {
+        [Required]
         public string Name { get; set; }
+
+        [Range(1, 1000)]
         public decimal HourRate { get; set; }
     }
 }
